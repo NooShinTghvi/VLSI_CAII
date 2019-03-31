c6e59879fc94043b3c129f584f7afe1616a9c86c
NooShin Taghavi <nooshin.tghvi@gmail.com>
2019-03-31 15:23:30 +0430

    Blahh,Blahh,Blahhh

--- ModelSim_Part1/TB.v
+++ ModelSim_Part1/TB.v
@@ -3,10 +3,18 @@ module A;
     reg [7:0] B;
     wire [15:0] P;
     part1 U( A, B, P );
-
     initial begin
-        $dumpfile ("testI.vcd");
-        $dumpvars;
-    
+        $dumpfile ("test3.vcd");
+        A   = 8'b01010101;
+        B   = 8'b10101010;
+        //#200
+        $dumpvars(0);
+        //$stop();
     end
+	//always #10 A = A + 1;
+    //always #20 B = B + 1;
+	//initial begin
+		
+	//	
+	//end
 endmodule
