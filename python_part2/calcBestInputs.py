e74a7a2e2ba284cf8625aeada1db02c49292e89b
NegarMirgati <negar.nbj95@gmail.com>
2019-04-01 12:31:23 +0430

    fix some bugs, print results in file and console

--- python_part2/calcBestInputs.py
+++ python_part2/calcBestInputs.py
@@ -3,6 +3,7 @@
  Negar Mirgati - Nooshin Taghavi
 '''
 import itertools
+import operator
 
 def getSortedLines() :
     sortedLines = []
@@ -47,9 +48,8 @@ def canBeCalculated(nodeDict, parts) :
             return 1
         else :
             return None
-
     else :
-        print('to be calculated first')
+        print('ERR occured')
         return None
 
 
@@ -58,19 +58,10 @@ def calcTotalLeakage(nodeDict, sortedLines) :
     for x in sortedLines :
             lineParts = x.split(' ')
             parts = x.split(' ')
-            val = calcProbsForNode(nodeDict, parts)
+            val = getLeakageOfGate(nodeDict, parts)
             leakageVal += val
-
     return leakageVal
                 
-
-
-# add wires as nodes to probability dict
-def createMapOfNodes(nodeDict, x) :
-    lineParts = x.replace('\n', '').replace('wire', '').replace(' ', '').split(',')
-    for node in lineParts :
-        nodeDict[node] = 0
-
 # add primary inputs to probability dict
 def addPrimaryInputsToDict(nodeDict, input) :
     counter = 0
@@ -82,11 +73,11 @@ def addPrimaryInputsToDict(nodeDict, input) :
         counter += 1
 
 # calculate output node values and leakage based on gate inputs
-def calcProbsForNode(nodeDict, parts) :
+def getLeakageOfGate(nodeDict, parts) :
     gate = parts[2]
     if(gate == 'INV_X1') :
         (inputNode, outputNode) = getInputAndOutputsOne(parts)
-        nodeDict[outputNode] = ~(nodeDict[inputNode])
+        nodeDict[outputNode] = inverter1(nodeDict[inputNode])
         return (calcLeakageOne(nodeDict[inputNode]))
 
 
@@ -102,8 +93,7 @@ def calcProbsForNode(nodeDict, parts) :
         return (calcLeakageTwo(gate, nodeDict[inputNode1], nodeDict[inputNode2]))
 
     else :
-        print('to be calculated first')
-        return None
+        print("ERR OCCURED!!!!!!!")
 
 # parse one input gate input and outputs
 def getInputAndOutputsOne(parts):
@@ -123,65 +113,90 @@ def getInputAndOutputsTwo(parts):
     outputNode = (s_out[s_out.find("(")+1:s_out.find(")")])
     return(inputNode1, inputNode2, outputNode)
 
-def nand2(A, B) :
-    return (~(A & B))
+def inverter1(A) :
+    if(A == 1) :
+        return 0
+    elif(A == 0):
+        return 1
+    else :
+        print("ERRRRRRRRRRR")
 
+def nand2(A, B) :
+    if(A is None or B is None):
+        return None
+    if(A == 1 and B == 1):
+        res = 0
+    else :
+        res = 1  
+    return res
+   
 def nor2(A, B): 
-    return (~(A | B))
+    if(A is None or B is None):
+        return None
+    if(A == 0 and B == 0) :
+        res = 1
+    else : 
+        res = 0 
+    return res
 
 
 # print calculated results in results.txt file
-def printResultsInOutputFile(nodeDict, alphaDict):
+def printResults(leakageValues, inputs):
     outfile = open('results.txt', 'w')
-    for key in nodeDict.keys():
-        line = (key + ' : ') + 'p(n = 1) : ' + str(nodeDict[key]) + ', p(n = 0) :' +  str(1 - nodeDict[key])
-        if(alphaDict.has_key(key)):
-            line += ', alpha : ' + str(alphaDict[key])
-        
+    sorted_x = sorted(leakageValues.items(), key=operator.itemgetter(1))
+    for i in range(0, 6):
+        index = sorted_x[i][0]
+        line = 'input : ' + inputs[index] + ' leakge = ' + str(sorted_x[i][1])
+        print(line)
         print >> outfile, line
+    #print(sorted_x[len(sorted_x)-1])
 
 
-# return leakage based on gate type and its inputs
+# return leakage based on gate type and its inputs for two input gates
 def calcLeakageTwo(gate, A, B) :
     if(gate == 'NAND2_X1'):
         if(A == 0 and B == 0):
             return 11
         elif ((A == 0 and B == 1) or (A == 1 and B == 0)):
             return 38
-        else:
+        elif(A == 1 and B == 1):
             return 58
-    else:
+        else :
+            print("ERR OCCURED")
+
+    elif(gate == 'NOR2_X1'):
         if(A == 0 and B == 0):
             return 47
         elif ((A == 0 and B == 1) or (A == 1 and B == 0)):
             return 30
-        else :
+        elif(A == 1 and B == 1) :
             return 25
+    else :
+        print("ERR OCCURED")
 
+# return leakage based on gate type and its inputs for one input gates
 def calcLeakageOne(A):
     if(A == 0):
         return 19
-    else : 
+    elif (A == 1)  : 
         return 18
+    else :
+        print("ERR OCCURED")
         
-
 def main() :
-    leakageValues = []
+    leakageValues = {}
+    inputs = []
     counter = 0
     sortedLines = getSortedLines()
     for x in (["".join(seq) for seq in itertools.product("01", repeat=16)]) :
         nodeDict = {}
         addPrimaryInputsToDict(nodeDict, x)
-        leakageValues.append(calcTotalLeakage(nodeDict, sortedLines))
+        leakageValues[counter] = calcTotalLeakage(nodeDict, sortedLines)
+        inputs.append(x)
         print(counter)
         counter += 1
     
-    sortedList = sorted(leakageValues)
-    for i in range(0, 6):
-        print (sortedList[i])
-    print (sortedList(len(sortedList) - 1))
-
-
+    printResults(leakageValues, inputs)
+  
 if __name__== "__main__":
       main()
-
